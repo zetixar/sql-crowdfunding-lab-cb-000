19047062e09a2042f4346f60baccab9b3ba7d3b7
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-29 14:16:46 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,54 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"
+"SELECT Projects.title, SUM(Pledges.amount)
+ FROM projects
+ JOIN pledges
+ ON Projects.id = Pledges.project_id
+ GROUP BY project_id
+ ORDER BY Projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
+"SELECT users.name, users.age, SUM(pledges.amount)
+   FROM Pledges
+   JOIN users
+   ON pledges.user_id = users.id
+   GROUP BY pledges.user_id
+   ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS difference
+ FROM Pledges
+ JOIN projects
+ ON pledges.project_id = projects.id
+ GROUP BY pledges.project_id
+ HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount)
+ FROM users
+ JOIN pledges
+ ON users.id = pledges.user_id
+ GROUP BY pledges.user_id
+ ORDER BY SUM(pledges.amount), Users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount
+ FROM pledges
+ JOIN projects
+ ON pledges.project_id = projects.id
+ WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+  "SELECT projects.category, SUM(pledges.amount)
+   FROM pledges
+   JOIN projects
+   ON pledges.project_id = projects.id
+   GROUP BY projects.category
+   HAVING projects.category = 'books';"
 end
